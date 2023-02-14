8c5535ff3b724edabb3427317ca9c81bd65c3edb
loloide <lorendlc2005@gmail.com>
2023-02-14 18:29:28 -0300

    added socket

--- App.tsx
+++ App.tsx
@@ -1,8 +1,48 @@
 import React from 'react';
 import {Text, View, StatusBar} from 'react-native';
 import { LatLng, LeafletView } from 'react-native-leaflet-view';
+import { io } from "socket.io-client";
 
-const HelloWorldApp = () => {
+// vars 
+
+var lineas: {
+  [x: string]: any; lon: any; 
+}[] = []
+var userlocation = {
+    lat: null,
+    lon: null,
+    acc: null,
+}
+var colors = {
+    bus1: "#FF00FF",
+    bus2: "#FFFF00",
+    bus3: "#00FFFF"
+}
+
+// Socket
+const socket = io("https://bondipredict.onrender.com/");
+
+socket.on("pos", function(data) {
+  if(lineas.some(linea => linea.bus === data.bus)){
+      var index = lineas.findIndex((obj => obj.bus == data.bus));
+      lineas[index].lat = data.lat
+      lineas[index].lon = data.lon
+  } else {
+      if (data.bus !== undefined){
+          lineas.push({
+              lat: data.lat,
+              lon: data.lon,
+              bus: data.bus
+          });
+      }
+  }  
+  console.log(lineas)
+})
+
+// Map
+
+// App
+const App = () => {
   return (
     <View
       style={{
@@ -12,9 +52,10 @@ const HelloWorldApp = () => {
       }}>
       <Text>Hello, world!</Text>
       <LeafletView
+
         // The rest of your props, see the list below
       />
     </View>
   );
 };
-export default HelloWorldApp;
+export default App;
