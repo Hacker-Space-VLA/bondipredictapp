aade8eea40690a6234c8b9e357f25e950b41f172
loloide <lorendlc2005@gmail.com>
2023-04-05 16:47:11 -0300

    started emitter app

--- App.tsx
+++ App.tsx
@@ -1,42 +1,28 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View, StatusBar, PermissionsAndroid, Button } from 'react-native';
+import {Text, View, StatusBar, PermissionsAndroid, Button, StyleSheet } from 'react-native';
 import { io } from "socket.io-client";
 import Geolocation from 'react-native-geolocation-service';
-import Leaflet, { Markers, TileOptions, Layers } from 'react-native-leaflet-ts'; 
+import { TextInput } from 'react-native-paper';
 
 // vars 
-const lineas: Markers[] = [];
 
 
 // Socket
 const socket = io("https://bondipredict.onrender.com/");
 
-socket.on("pos", function(data) {
-  if(lineas.some(linea => linea.title === data.bus)){
-    for (var bus of lineas) {
-      if (bus.title = data.bus) {
-        bus.latLng = [parseFloat(data.lat) , parseFloat(data.lon)]
-
-      }
-    }
-      
-  } else {
-      if (data.bus !== undefined){
-          lineas.push({
-              latLng: [parseFloat(data.lat) , parseFloat(data.lon)],
-              title: data.bus,
-              iconSize: {
-                width: 25,
-                height: 25,
-              },
-          });
-      }
+setInterval(()=>{
+  getLocation()
+  var data = {
+    lat: usrLocation.lat,
+    lon: usrLocation.lng,
+    bus: bus,
+    key: "dsaasddsaasd",
+    id: socket.id
   }
-})
-
+  socket.emit("pos",data)
+  console.log(data)
+}, 1000)
 // Location
-
-
 const requestLocationPermission = async () => {
 
   console.log("--------------------------------------------------------------------------------------------------------------")
@@ -98,63 +84,33 @@ function getLocation() {
   
 }
 
-// Map
-const options: TileOptions = {
-  noWrap: true,
-  updateInterval: 100
-};
-
-const mapLayers: Layers[] = [
-  {
-    name: 'Floor 1',
-    src: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-    tileOptions: options,
-  }
-];
-
-
-var moved = false
+var bus = "x"
 // App
 const App = () => {
-  var [loc, setLoc] = useState([usrLocation.lat, usrLocation.lng]);
+  const [text, setText] = React.useState("");
   
-  const askloc = async () => {
-    //console.log("ask loc")
-    const result = await getLocation()
-    console.log("loc: ", result)
-    return(result)
-  }
-
-  if (moved == false) {
-    askloc().then(function() {
-      setLoc([usrLocation.lat, usrLocation.lng])
-      moved = true
-    })
-  }
-
-  var [buses, setBuses] = useState(lineas)
-
-  
-  
+  bus = text
   return (
-      <Leaflet
-      mapLayers={mapLayers}
-      minZoom={2}
-      flyTo={{
-        latLng: loc,
-        zoom: 12,
-      }}
-      markers={buses}
-      backgroundColor="#b0d4dc"
-      injectJavascript={(
-        useEffect(() => {
-          setInterval(()=> {
-            setBuses(lineas)
-            console.log("buses: ", buses)
-          }, 1000)
-        }, [buses])
-      )}
-    />
+    <View>
+      <Text>bus: {text}</Text>
+      <View>
+        <TextInput
+          label="bus"
+          value={text}
+          onChangeText={text => setText(text)}
+          mode="outlined"
+        />
+      </View>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  locview: {
+    backgroundColor: "#3e3e3e",
+    justifyContent: 'center',
+    alignItems: 'center'
+    
+  }
+})
 export default App;
